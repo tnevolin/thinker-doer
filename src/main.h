291e4248058814e6bcb07427116d7d0bc0d19962
Tim Nevolin <tnevolin@gmail.com>
2020-01-18 23:42:14 -0500

    Disabled homebase CC morale bonus.

--- src/main.h
+++ src/main.h
@@ -130,6 +130,7 @@ struct Config {
     bool sea_territory_distance_same_as_land = false;
     bool coastal_territory_distance_same_as_sea = false;
     bool alternative_artillery_damage = false;
+    bool disable_homebase_cc_morale_bonus = false;
 };
 
 /*
