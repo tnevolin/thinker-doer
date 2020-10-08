e8f13bdbc0d79e93e6f8db28cd47965e9375d43b
Tim Nevolin <tnevolin@gmail.com>
2020-10-07 21:31:51 -0400

    #155

--- src/main.h
+++ src/main.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifdef BUILD_REL
-    #define MOD_VERSION "The Will to Power mod - version 154"
+    #define MOD_VERSION "The Will to Power mod - version 155"
 #else
     #define MOD_VERSION "The Will to Power mod - development"
 #endif
